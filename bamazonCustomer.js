21909e476fcbe00ab0edd7968c039813086276ed
beaker113 <43103384+beaker113@users.noreply.github.com>
2019-02-04 16:27:08 -0800

    trying to fix the updated quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,31 +14,48 @@ connection.connect(function (err) {
     console.log("conected")
 });
 
-connection.query("SELECT * FROM PRODUCTS", function(err, rows, fields) {
+connection.query("SELECT * FROM PRODUCTS", function (err, rows, fields) {
     console.log(rows)
 })
 inquirer
-.prompt([{
-    name:"id",
-    message:"what is the ID of the product you would like to buy?"
-},
-{
-    name:"quantity",
-    message:"what is the quantity you would like to purchase?"
-}])
-.then(function( answers) {
-    connection.query("SELECT * FROM PRODUCTS WHERE ID =" + answers.id, function(err, res) {
-        if (err) throw err;
-        console.log(res)
-    });
-    connection.query(
-        'UPDATE products SET quantity = ? Where ID = ?',
-        [Number("quantity") - Number(answers.quantity), answers.id],
-        function(err, result)  {
-            if (err) throw err;
-        
-            console.log(`Changed ${result.changedRows} row(s)`);
-          }
+    .prompt([{
+        type: "input",
+        name: "id",
+        message: "what is the ID of the product you would like to buy?"
 
-        );
-})
+    },
+    {
+        type: "input",
+        name: "quantity",
+        message: "what is the quantity you would like to purchase?"
+    }])
+    .then(function (answers) {
+        connection.query("SELECT * FROM products WHERE ID = " + answers.id, function (err, res) {
+            if (err) throw err;
+            console.log(res)
+        });
+        var product = answers.id
+        var amount = Number(answers.quantity)
+        connection.query("SELECT quantity FROM products WHERE ID = " + product, function (err, res) {
+            if (err) throw err;
+            var quantity = Number(res);
+            
+             if(quantity > amount)
+         {
+            connection.query(
+                'UPDATE products SET quantity = ' + (quantity - amount) + 'Where ID = ' + product,
+                // [(Number("quantity") - amount), product],
+                function (err, result) {
+                    if (err) throw err;
+                    
+
+                    console.log(`Changed ${result.changedRows} row(s)`);
+                    console.log(result)
+                }
+
+            );
+        }
+        if (amount > quantity) { console.log("Sorry, we do not have that amount in stock") }
+    })
+
+    })
